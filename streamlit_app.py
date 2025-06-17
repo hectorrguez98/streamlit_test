b09d797d10c7771f7c5962a62306347a0475d9ee
Héctor Ramírez <hector10218@gmail.com>
2025-06-17 17:31:07 -0600

    Corrijo nombre del archivo config.toml para Streamlit

---
+++ streamlit_app.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import scipy.stats
+import streamlit as st
+import time
+
+# Variables de estado para mantener datos entre ejecuciones
+if 'experiment_no' not in st.session_state:
+    st.session_state['experiment_no'] = 0
+
+if 'df_experiment_results' not in st.session_state:
+    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
+
+st.header('Lanzar una moneda')
+
+# Gráfico inicial
+chart = st.line_chart([0.5])
+
+# Función que simula lanzamientos de moneda
+def toss_coin(n):
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
+
+    mean = None
+    outcome_no = 0
+    outcome_1_count = 0
+
+    for r in trial_outcomes:
+        outcome_no += 1
+        if r == 1:
+            outcome_1_count += 1
+        mean = outcome_1_count / outcome_no
+        chart.add_rows([mean])
+        time.sleep(0.05)
+
+    return mean
+
+# Widgets de la interfaz
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
+
+# Si se presiona el botón
+if start_button:
+    st.write(f'Experimento con {number_of_trials} intentos en curso.')
+    st.session_state['experiment_no'] += 1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([
+        st.session_state['df_experiment_results'],
+        pd.DataFrame(data=[[st.session_state['experiment_no'], number_of_trials, mean]],
+                     columns=['no', 'iteraciones', 'media'])
+    ], axis=0).reset_index(drop=True)
+
+# Mostrar tabla con resultados
+st.write(st.session_state['df_experiment_results'])
